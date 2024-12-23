00c1bff478898a0d3b1b2770327014999a507d15
Lee Jin Woo <110000734+ev1025@users.noreply.github.com>
2024-12-23 20:40:41 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,6 +1,5 @@
 def solution(rsp):
-    win_map = {'2': '0', '0': '5', '5': '2'}
-    
-    return ''.join([win_map[ch] for ch in rsp])
+    rsp_ = {'2':'0', '0':'5','5':'2'}
+    return ''.join(rsp_[i] for i in rsp)
 
 
