3503dac0e60d172528c10d746308131d711c5fd3
Lee Jin Woo <110000734+ev1025@users.noreply.github.com>
2024-12-31 14:50:00 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,6 +1,6 @@
 def solution(arr, idx):
-    for i in range(len(arr)):
-        if (i >= idx) and (arr[i] == 1):
+    for i in range(idx, len(arr)):
+        if (arr[i] == 1):
             return i
     return -1
   
