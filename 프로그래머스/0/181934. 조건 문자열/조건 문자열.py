a410edd8126c261f4dd887b2bf27ccb9b53393b4
Lee Jin Woo <110000734+ev1025@users.noreply.github.com>
2025-02-18 17:42:33 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -0,0 +1,10 @@
+def solution(ineq, eq, n, m):
+    if ineq == ">" and eq=="=":
+        return int(n>=m)
+    if ineq == "<" and eq=="=":
+        return int(n<=m)
+    if ineq == ">" and eq=="!":
+        return int(n>m)
+    if ineq == "<" and eq=="!":
+        return int(n<m)
+            
